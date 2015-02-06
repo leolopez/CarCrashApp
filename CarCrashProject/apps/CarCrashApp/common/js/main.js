30f9b13fa8cdeb906e7800cff21e49a857d0021d
leo <leo@leo-HP>
2015-02-06 15:26:27 -0600

    updated main.js

--- CarCrashProject/apps/CarCrashApp/common/js/main.js
+++ CarCrashProject/apps/CarCrashApp/common/js/main.js
@@ -13,6 +13,8 @@ function wlCommonInit(){
 	
 	// Common initialization code goes here
 	
+	
+	
 	var pages = getPages(); //pages object
 	$.each(pages, function(idx, obj){
 		$.get(obj.url, function(data){
@@ -75,11 +77,14 @@ function wlCommonInit(){
 
 function initializeData()
 {
+	
 	location.href = '#login'; //First page to show
 	$('a').attr('data-transition','slide');	//general app transition
 	$("a[href='#sinisterReport']").click(function(){
     	getLocation();
     });
+	
+	initLanguage();
 }
 
 //Pages array to load on index
@@ -241,4 +246,202 @@ function getPages()
 	        	}
 	        	}
 	];
+	
+}
+
+
+function initLanguage(){
+	 var locale = WL.App.getDeviceLocale();
+	    var lang = WL.App.getDeviceLanguage();
+	    WL.Logger.debug(">> Detected locale: " + locale);
+	    WL.Logger.debug(">> Detected language: " + lang);
+
+	    if (locale.indexOf("en")!=-1) languageChanged("english");
+	    if (locale.indexOf("es")!=-1) languageChanged("spanish");
+}
+function languageChanged(lang) {		
+    switch (lang){
+    	case "english":
+    		setEnglish();
+    		break;
+    	case "spanish":
+    		setSpanish();
+    		break;
+    }
+    
+    initProfileTranslations();
+    initShowPoliciesTranslations();
+    initMedicalDataTranslations();
+    initMechanicDataTranslations();
+    initAddPolicyTranslations();
+    initReportSinisterTranslations();
+    initReportSinisterDetTranslations();
+    initSinisterListTranslations();
+    initTheftsListTranslations();
+    initPoliciesAlertsTranslations();
+    initContactsTranslations();
+    initLeftPanelTranslations();
+    initSingUpTranslations();
+    initLogInTranslations();
+}
+
+function initProfileTranslations (){
+	 $("#myAccName").text(Messages.myAccName);
+	    $("#enterDetails").text(Messages.enterDetails);
+	    $("#lblProfileName").text(Messages.lblProfileName);
+	    $("#lblFirstName").text(Messages.lblFirstName);
+	    $("#lblLastName").text(Messages.lblLastName);
+	    $("#lblCellPhone").text(Messages.lblCellPhone);
+	    $("#lblCityRes").text(Messages.lblCityRes);
+	    $("#lblCompany").text(Messages.lblCompany);
+	    $("#h1ConfirmCity").text(Messages.h1ConfirmCity);
+	    $("#h3SelectedCity").text(Messages.h3SelectedCity);	
+}
+function initShowPoliciesTranslations(){
+	  $("#h3RecordsList").text(Messages.h3RecordsList);
+	    $("#h1ConfirmPolicy").text(Messages.h1ConfirmPolicy);
+	    $("#h3DeletePolicy").text(Messages.h3DeletePolicy);
+	    $("#aAddPolicy").text(Messages.aAddPolicy);
+	    $("#aDeletePolicy").text(Messages.aDeletePolicy); 
+}
+
+function initMedicalDataTranslations(){
+	 $("#lblIMSSNum").text(Messages.lblIMSSNum); 
+	    $("#lblBloodType").text(Messages.lblBloodType); 
+	    $("#lblAllergies").text(Messages.lblAllergies); 
+	    $("#lblConditions").text(Messages.lblConditions); 
+	    $("#h3MedicalData").text(Messages.h3MedicalData); 
+}
+function initMechanicDataTranslations(){
+	 $("#h3MechanicDetails").text(Messages.h3MechanicDetails); 
+	    $("#lblMechanicName").text(Messages.lblMechanicName); 
+	    $("#lblMechanicFirstName").text(Messages.lblMechanicFirstName); 
+	    $("#lblMechanicLastName").text(Messages.lblMechanicLastName); 
+	    $("#lblMechanicCellPhone").text(Messages.lblMechanicCellPhone); 
+	    $("#lblMechanicAddress").text(Messages.lblMechanicAddress); 
+}
+function initAddPolicyTranslations(){
+	 $("#h3AddPolicies").text(Messages.h3AddPolicies); 
+	    $("#lblPolicyNum").text(Messages.lblPolicyNum); 
+	    $("#lblPolicyExpiration").text(Messages.lblPolicyExpiration); 
+	    $("#opNoneInsurance").text(Messages.opNoneInsurance); 
+	    $("#lblInsurance").text(Messages.lblInsurance);
+	    $("#txtPolicyDate").attr("placeholder", Messages.placeholderDateFormat); 
+	    $("#txtAseg").val(Messages.txtAseg);  
+	    $("#aRecordsList").text(Messages.aRecordsList); 
+	    //Vehicle section
+	    $("#h3Vehicle").text(Messages.h3Vehicle); 
+	    $("#lblPlates").text(Messages.lblPlates); 
+	    $("#lblSeries").text(Messages.lblSeries); 
+	    $("#lblVehicleType").text(Messages.lblVehicleType); 
+	    $("#lblMark").text(Messages.lblMark); 
+	    $("#lblSubMark").text(Messages.lblSubMark); 
+	    $("#lblModel").text(Messages.lblModel); 
+	    $("#lblColor").text(Messages.lblColor); 
+	    $("#aTakeCarPict").text(Messages.aTakeCarPict); 
+	    $("#h1ConfirmMark").text(Messages.h1ConfirmMark); 
+	    $("#pMark").text(Messages.pMark); 
+	    $("#lblOwnerName").text(Messages.lblOwnerName); 
+	    $("#pSubMark").text(Messages.pSubMark); 
+	    $("#h3SelectedMark").text(Messages.h3SelectedMark); 
+}
+
+function initReportSinisterTranslations(){
+	 $("#h3ReportIncident").text(Messages.h3ReportIncident); 
+	    $("#aActualizeLocation").text(Messages.aActualizeLocation); 
+	    $("#opSelectCar").text(Messages.opSelectCar); 
+	    $("#aRepSinister").text(Messages.aRepSinister); 
+	    $("#aRepTheft").text(Messages.aRepTheft);  
+}
+
+function initReportSinisterDetTranslations(){
+	 $("#aTakeSinisterPic").text(Messages.aTakeSinisterPic); 
+	    $("#lblflipAmbulancia").text(Messages.lblflipAmbulancia); 	    
+	    $("#opOff").text(Messages.opOff); 
+	    $("#opOn").text(Messages.opOn); 
+	    $("#lblflipLegal").text(Messages.lblflipLegal);  
+	    $("#opflipLegalOff").text(Messages.opOff); 
+	    $("#opflipLegalOn").text(Messages.opOn);
+	    $("#lblObservaciones").text(Messages.lblObservaciones);
+	    $("#aSendInfo").text(Messages.aSendInfo);
+}
+
+function initSinisterListTranslations(){
+	 $("#h3SinisterList").text(Messages.h3SinisterList); 
+	    $("#aSinisterList").text(Messages.aSinisterList); 	    
+	    $("#aTheftList").text(Messages.aTheftList); 
+	    $('span[dat="listDate"]').text(Messages.date);
+	    $('span[rep="sinister"]').text(Messages.report);
+}
+
+function initTheftsListTranslations(){
+	 $("#h3TheftsList").text(Messages.h3TheftsList); 
+	    $("#aSinisterList2").text(Messages.aSinisterList); 	    
+	    $("#aTheftList2").text(Messages.aTheftList); 	   
+}
+
+function initPoliciesAlertsTranslations(){
+	 $("#h3PoliciesAlerts").text(Messages.h3PoliciesAlerts); 
+	    $("#pExpiredPolicy").text(Messages.pExpiredPolicy);    
+}
+
+function initContactsTranslations(){
+	 $("#h3ContactsReport").text(Messages.h3ReportIncident);    
+}
+
+function initLeftPanelTranslations(){	
+	 $('h2[tit="h2Profile"]').text(Messages.h2Profile);  	 	 
+	 $('a[sel="aAccount"]').text(Messages.aAccount);  	 	 
+	 $('a[sel="aPoliza"]').text(Messages.aPoliza);  	 	 
+	 $('a[sel="aMedicalData"]').text(Messages.aMedicalData);  	 	 
+	 $('a[sel="aMechanicContact"]').text(Messages.aMechanicContact);  	 	 
+	 $('h2[tit="aSinister"]').text(Messages.aSinister);  	 	 
+	 $('a[sel="aLiReportIncident"]').text(Messages.h3ReportIncident);  	 	 
+	 $('a[sel="aLiTheftsList"]').text(Messages.aTheftList);  	 	 
+	 $('a[sel="aLiSinistersList"]').text(Messages.aSinisterList);  	 	 
+	 $('a[sel="aLiAlerts"]').text(Messages.aLiAlerts);  	 	 
+	 $('a[sel="aContacts"]').text(Messages.aContacts);  	 	 	 	   
+}
+
+function initSingUpTranslations(){	
+	 $('#h2SignUp').text(Messages.h2SignUp); 
+	 $('#txtName').attr("placeholder", Messages.txtName); 
+	 $('#txtNacimiento').attr("placeholder", Messages.txtNacimiento); 
+	 $('#txtPais').attr("placeholder", Messages.txtPais); 
+	 $('#txtCiudad').attr("placeholder", Messages.txtCiudad); 
+	 $('#txtEmail').attr("placeholder", Messages.txtEmail); 
+	 $('#txtPass').attr("placeholder", Messages.txtPass); 
+	 $('#txtPassConfirm').attr("placeholder", Messages.txtPassConfirm); 
+	 $('#txtCelular').attr("placeholder", Messages.txtCelular); 
+	 $('#aSignUp').text(Messages.h2SignUp); 
+	 $('#aSignIn').text(Messages.aSignIn); 
+}
+
+function initSingUpTranslations(){	
+	 $('#h2SignUp').text(Messages.h2SignUp); 
+	 $('#txtName').attr("placeholder", Messages.txtName); 
+	 $('#txtNacimiento').attr("placeholder", Messages.txtNacimiento); 
+	 $('#txtPais').attr("placeholder", Messages.txtPais); 
+	 $('#txtCiudad').attr("placeholder", Messages.txtCiudad); 
+	 $('#txtEmail').attr("placeholder", Messages.txtEmail); 
+	 $('#txtPass').attr("placeholder", Messages.txtPass); 
+	 $('#txtPassConfirm').attr("placeholder", Messages.txtPassConfirm); 
+	 $('#txtCelular').attr("placeholder", Messages.txtCelular); 
+	 $('#aSignUp').text(Messages.h2SignUp); 
+	 $('#aSignIn').text(Messages.aSignIn); 
+	 
+}
+
+function initLogInTranslations(){	
+	 $('#btnLogIn').val(Messages.aSignIn); 
+	 $('#aLogSignUp').text(Messages.h2SignUp);
+	 $('#txtPwd').attr("placeholder", Messages.txtPass);
+	 $('#aHelp').text(Messages.aHelp);
+}
+
+function initSinisterPopUpTranslations(){
+	//use this function in pagebeforeshow 
+	$('h1[h1pop="popHeader"]').text(Messages.popHeader);
+	$('h3[h3pop="popTitle"]').text(Messages.popTitle);
+	$('p[ppop="popContent"]').text(Messages.popContent);
 }
