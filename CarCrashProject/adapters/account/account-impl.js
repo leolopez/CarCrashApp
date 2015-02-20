4310b4e98e3ddb5f14d2ae0575c35153caf6809c
dramirezh <dramirez@soluiaintelligence.com>
2015-02-20 15:56:55 -0600

    adapter upload

---
+++ CarCrashProject/adapters/account/account-impl.js
@@ -0,0 +1,21 @@
+/**
+ * @param objAccount
+ *            must be an object of account class from js/account.js
+ * @returns json true if registration was successful false if it isn't
+ */
+function registerAccount(objAccount)
+{
+	var ret = {value:true};
+	return ret;
+}
+
+/**
+ * @param objAccount
+ *            must be an object of account class from js/account.js
+ * @returns json account object of user / null if login was incorrect
+ */
+function verifyLogin(objAccount)
+{
+	var ret = {value:true};
+	return ret;
+}
