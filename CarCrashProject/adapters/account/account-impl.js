e2d01f974d9deaaeaa1c2f41964b4582eff23375
dramirezh <dramirez@soluiaintelligence.com>
2015-03-02 18:47:27 -0600

    account Adapter to SQL

--- CarCrashProject/adapters/account/account-impl.js
+++ CarCrashProject/adapters/account/account-impl.js
@@ -1,39 +1,29 @@
-/**
- * @param objAccount
- *            must be an object of account class from js/account.js
- * @returns json true if registration was successful false if it isn't
+var addStatement = WL.Server.createSQLStatement("insert into Accounts (FirstName, BirthDate, Email, CellPhone, Password) values (?, ?, ?, ?, ?)");
+var selectStatement = WL.Server.createSQLStatement("select FirstName AS 'firstName', LastName As 'lastName', SecondLastName As 'secondLastName', BirthDate As 'birthDate', Country AS 'country', State AS 'state', City AS 'city', Email AS 'email', CellPhone AS 'cellPhone', Password AS 'password' FROM Accounts WHERE Email = ? AND Password = ?");
+
+/************************************************************************
+ * Implementation code for procedure - 'procedure1'
+ *
+ * @param - email and passwor to validate
+ * @return - invocationResult
  */
-function registerAccount(objAccount)
-{
-	var ret = {value:true};
-	return ret;
+function accessAccount(pData) {
+	return WL.Server.invokeSQLStatement({
+		preparedStatement : selectStatement,
+		parameters : [pData.email, pData.password]
+	});
 }
 
-/**
- * @param objAccount
- *            must be an object of account class from js/account.js
- * @returns json account object of user / null if login was incorrect
+/************************************************************************
+ * Implementation code for procedure - 'procedure2'
+ *
+ * @param - account object from js/account.js
+ * @return - invocationResult
  */
-function verifyLogin(pData)
-{
-	var oAcc = {
-			data:{
-				firstName: "Diego",
-				lastName: "",
-				birthDay: "1991-03-10",
-				country: "",
-				state: "",
-				city: "",
-				email: "123@123.com",
-				cellPhone: "123123123123",
-				password: "123123"
-			}
-	};
-	
-	if(oAcc.data.email == pData.email && oAcc.data.password == pData.password){
-		return oAcc;
-	}
-	else{
-		return {};
-	}
+function saveAccount(account) {
+	return WL.Server.invokeSQLStatement({
+		preparedStatement : addStatement,
+		parameters : [account.firstName, account.birthDate, account.email, account.cellPhone, account.password]
+	});
 }
+
