2e99b34a93c07e9a25a562f7abb93eb671c91910
dramirezh <dramirez@soluiaintelligence.com>
2015-03-02 19:05:56 -0600

    add collections to collections.js

---
+++ CarCrashProject/apps/CarCrashApp/common/js/JSonStore/collections/collections.js
@@ -0,0 +1,54 @@
+;(function () {
+
+	WL.JSONStore.init({
+		perfil : {
+			searchFields: {email:"string", password:"string"}
+		},
+		Contacts:{
+			searchFields:{UserContactName:'string',		
+				UserContactFirstName:'string',UserContactLastName:'string',
+				UserContactCellPhon:'string' }
+		},
+		reports:{
+			searchFields:{
+				idPolicy:'string', date:'string', status:'boolean', type:'string'
+			}
+		},
+		reportExtras:{
+			searchFields:{
+				idReport:"integer"
+			}
+		},
+		profile : {
+	        searchFields: {mobileId: 'string', name: 'string', firstname: 'string', lastname: 'string', cellPhone: 'string',
+	        	city: 'string', enterprise: 'string',birthDate: 'string',email: 'string',password: 'string'}
+	    },
+	    PolicyVehicle : {
+	        searchFields: {mobileId: 'string',PolicyNo: 'string', PolicyDate: 'string', insurance: 'string', Plates: 'string', Serie: 'string'
+	        	, VehicleType: 'string', Mark: 'string', SubMark: 'string', Model: 'string', Color: 'string'
+	         		, carPicture: 'string', Holder: 'string', OwnerCellPhone: 'string', PolicyContactName:'string',
+						PolicyContactFirstName:'string', PolicyContactLastName:'string',
+						PolicyContactCellPhon:'string'
+	         	}
+	     },
+	     MedicalData : {
+	         searchFields: {IMSS: 'string', bloodType: 'string', alergics: 'string', clinicalConditions: 'string'
+	         	}
+	     },
+	     MechanicData : {
+	         searchFields: { MechanicName: 'string',  MechanicFirstName: 'string', MechanicLastName: 'string', MechanicCellPhone: 'string',
+	         	MechanicAddress: 'string'}
+	     }
+	}, {
+		// password : 'PleaseChangeThisPassword'
+	})
+
+	.then(function () {
+		WL.Logger.debug(['All collections was loaded successfully'].join('\n'));
+	})
+
+	.fail(function (errObj) {
+		WL.Logger.ctx({pretty: true}).error(errObj);
+	});
+
+}());
