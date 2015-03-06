1f5e61a0915fd44499efa5e5553e8bb8b02e7472
leo <leo@leo-HP>
2015-03-06 17:25:47 -0600

    updated lastname space

--- CarCrashProject/apps/CarCrashApp/common/js/perfil.js
+++ CarCrashProject/apps/CarCrashApp/common/js/perfil.js
@@ -220,7 +220,7 @@ initLeftPanelTranslations()
        
         	 
         	 $('#'+nameInput).keypress(function(key) {        		
-        		 if((key.charCode < 97 || key.charCode > 122) && (key.charCode < 65 || key.charCode > 90)
+        		 if((key.charCode < 97 || key.charCode > 122) && (key.charCode < 65 || key.charCode > 90) && (key.charCode != 32)
         				 && (key.charCode != 225)&& (key.charCode != 233)&& (key.charCode != 237)&& (key.charCode != 243)&& (key.charCode != 250)&& (key.charCode != 241)
         	   				&& (key.charCode != 193)&& (key.charCode != 201)&& (key.charCode != 205)&& (key.charCode != 211)&& (key.charCode != 218)&&  (key.charCode != 209)		 
         		 ){
